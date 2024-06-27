41fa19576042990e512e9cafff8bde9aec02b1fc
rogerio <rogeriopiatek@gmail.com>
2024-06-27 20:19:58 -0300

    feat: add Update function for Todo

---
+++ models/update.go
@@ -0,0 +1,21 @@
+package models
+
+import "github.com/rogeriopiatek/goLang-todoAPI/db"
+
+// Updates an Todo based on the ID passed to this function
+func Update(id int64, todo Todo) (int64, error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	res, err := conn.Exec(`UPDATE todos SET title=$2, description=$3, done=$4 WHERE id=$1`, id, todo.Title, todo.Description, todo.Done, todo.ID)
+	if err != nil {
+		return 0, err
+	}
+
+	//returns the number of rows affected (an Int)
+	//useful on the handler, we validate if UPDATE only changes one Row (because one ID was passed)
+	return res.RowsAffected()
+}
