58c2639a21aa153f759172171879e4d8d381a802
rogerio <rogeriopiatek@gmail.com>
2024-06-27 21:26:36 -0300

    feat: add Delete handler to ensure a Todo can be deleted

--- models/delete.go
+++ models/delete.go
@@ -3,7 +3,7 @@ package models
 import "github.com/rogeriopiatek/goLang-todoAPI/db"
 
 // Updates an Todo based on the ID passed to this function
-func Delete(id int64, todo Todo) (int64, error) {
+func Delete(id int64) (int64, error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
 		return 0, err
