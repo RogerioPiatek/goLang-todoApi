ce84afa426a6dabbcab1670ca8c69f170c5c945b
rogerio <rogeriopiatek@gmail.com>
2024-06-27 21:30:43 -0300

    feat: add Get handler to ensure one Todo can be retrieved

---
+++ handlers/get.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/rogeriopiatek/goLang-todoAPI/models"
+)
+
+// HTTP endpoint that retrives one Todo based on the ID passed to the URL parameter
+func Get(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
+	if err != nil {
+		log.Printf("Erro ao fazer parse do id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	todo, err := models.Get(id)
+	if err != nil {
+		log.Printf("Erro ao atualizar registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	//directly encodes our returned Todo
+	json.NewEncoder(w).Encode(todo)
+}
